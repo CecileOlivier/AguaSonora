66175f04a94d11cd7e614dc928b95a1249d9f527
Cecile <cecile.olivier56@gmail.com>
2014-11-23 16:36:18 +0100

    definition du config

---
+++ config-example.php
@@ -0,0 +1,12 @@
+<?php
+$config = array();
+$config['nb_music'] = 6;
+$config['nb_date_future'] = 4;
+$config['nb_date_passee'] = 2;
+$config['host'] = '...';
+$config['dbname'] = '...';
+$config['user'] = '...';
+$config['password'] = '...';
+$config['name_admin'] = '...';
+$config['pass_admin'] = '...';
+?>
