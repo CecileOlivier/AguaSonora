e56e686b49d1a526afbb4aee169f5706da2075da
Cecile <cecile.olivier56@gmail.com>
2014-11-23 16:37:41 +0100

    ajout de la connexion à la base de donnée

---
+++ init.php
@@ -0,0 +1,20 @@
+<?php 
+require('function.php');
+require('config.php');
+
+session_start();
+try
+{
+$dsn = 'mysql:host='.$config['host'].';dbname='.$config['dbname'].';charset=utf8';
+$utilisateur = $config['user'];
+$motDePasse = $config['password'];
+  $options = array(
+    PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8",
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+  );
+  $connexion = new PDO( $dsn, $utilisateur, $motDePasse, $options );
+  $connexion->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+} catch ( Exception $e ) {
+  echo "Connexion à MySQL impossible : ", $e->getMessage();
+  die();
+}
